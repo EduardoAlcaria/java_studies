31bebb29e3b424308aa1cc848f8cb611523a3d27
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-01-17 08:39:10 -0300

    Lesson 2 about primitive types

---
+++ theory/HelloIntelij/src/academy/devdojo/maratonajava/introduction/Lesson02PrimitiveTypes.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.introduction;
+
+public class Lesson02PrimitiveTypes {
+    public static void main(String[] args) {
+        //int, double, float, char, byte, short, long, boolean
+        int age = 10;
+        System.out.println("the variable age is "+age+" years old");
+    }
+}
