732d503a4d524d3d4787b809e609f42f0639c8f6
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-01-17 09:08:50 -0300

    studies about all the primitive types

--- theory/HelloIntelij/src/academy/devdojo/maratonajava/introduction/Lesson02PrimitiveTypes.java
+++ theory/HelloIntelij/src/academy/devdojo/maratonajava/introduction/Lesson02PrimitiveTypes.java
@@ -4,6 +4,16 @@ public class Lesson02PrimitiveTypes {
     public static void main(String[] args) {
         //int, double, float, char, byte, short, long, boolean
         int age = 10;
+        long hugeNumber = 1000000;
+        double salaryDouble = 2000;
+        float salaryFloat = 2500;
+        byte ageByte = 10;
+        short ageShort = 10;
+        boolean isTrue = true;
+        boolean isFalse = false;
+        char letter = 'A'; //can be used with Unicode or even the ascii table
         System.out.println("the variable age is "+age+" years old");
+        System.out.println(isTrue);
+        System.out.println("Char "+letter);
     }
 }
