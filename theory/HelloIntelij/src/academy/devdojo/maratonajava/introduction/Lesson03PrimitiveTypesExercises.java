96869ef589e9eb743c52dc0e11f218dc062c6d14
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-01-17 10:54:59 -0300

    exercises involving the topics from the lessons 1 and 2

---
+++ theory/HelloIntelij/src/academy/devdojo/maratonajava/introduction/Lesson03PrimitiveTypesExercises.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introduction;
+
+public class Lesson03PrimitiveTypesExercises {
+    public static void main(String[] args) {
+        String name = "Eduardo Alcaria Lopes";
+        String address = "231 Grand Avenue, NY";
+        double salary = 2500.0;
+        String payDay = "06/12/2025";
+        String message = "i "+name+" living at "+address+" confirm that i have received the payment of "+salary+"$"+" on "+payDay;
+        System.out.println(message);
+    }
+}
