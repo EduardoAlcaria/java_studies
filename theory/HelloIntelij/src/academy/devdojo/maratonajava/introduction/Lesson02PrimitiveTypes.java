de1b9dcd67d3f5a657fc16fe35a2d9fe722e1b21
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-01-17 09:55:22 -0300

    studies about primitive types and the string class

--- theory/HelloIntelij/src/academy/devdojo/maratonajava/introduction/Lesson02PrimitiveTypes.java
+++ theory/HelloIntelij/src/academy/devdojo/maratonajava/introduction/Lesson02PrimitiveTypes.java
@@ -5,15 +5,17 @@ public class Lesson02PrimitiveTypes {
         //int, double, float, char, byte, short, long, boolean
         int age = 10;
         long hugeNumber = 1000000;
-        double salaryDouble = 2000;
-        float salaryFloat = 2500;
+        double salaryDouble = 2000.0;
+        float salaryFloat = 2500.0F;
         byte ageByte = 10;
         short ageShort = 10;
         boolean isTrue = true;
         boolean isFalse = false;
+        String name = "Eduardo Alcaria Lopes";
         char letter = 'A'; //can be used with Unicode or even the ascii table
         System.out.println("the variable age is "+age+" years old");
         System.out.println(isTrue);
         System.out.println("Char "+letter);
+        System.out.println("name "+name);
     }
 }
