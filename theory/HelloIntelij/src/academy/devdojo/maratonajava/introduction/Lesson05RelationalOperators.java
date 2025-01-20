a5ce77970921a169e8bb0120a1214c0fe0975072
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-01-20 08:45:58 -0300

    studies about relational operators

---
+++ theory/HelloIntelij/src/academy/devdojo/maratonajava/introduction/Lesson05RelationalOperators.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratonajava.introduction;
+
+public class Lesson05RelationalOperators {
+    public static void main (String[] args){
+        int oddOrEven = 21 % 2;
+        System.out.println(oddOrEven);
+    }
+}
