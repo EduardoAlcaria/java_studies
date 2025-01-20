ca595dc83b2e64b937aa6c0bdd8f75f986e6f7f7
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-01-20 08:52:58 -0300

    studies about logic operators

---
+++ theory/HelloIntelij/src/academy/devdojo/maratonajava/introduction/Lesson06LogicOperators.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.introduction;
+
+public class Lesson06LogicOperators {
+    public static void main (String[] args) {
+        boolean isTenHigherThen20 = 10 > 20;
+        boolean isTenLowerThen20 = 10 < 20;
+        boolean isTenIgual20 = 10 == 20;
+        boolean isTenDifferent20 = 10 != 20;
+        System.out.println("isTenHigherThen20 "+isTenHigherThen20);
+        System.out.println("isTenLowerThen20 "+isTenLowerThen20);
+        System.out.println("isTenIgual20 "+isTenIgual20);
+        System.out.println("isTenDifferent20 "+isTenDifferent20);
+    }
+}
