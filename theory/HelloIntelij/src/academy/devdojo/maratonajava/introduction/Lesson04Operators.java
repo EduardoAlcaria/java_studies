e7018e0b16045ccdb3207bcaae6b506460794a97
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-01-20 08:24:57 -0300

    studies about arithmetics operators

---
+++ theory/HelloIntelij/src/academy/devdojo/maratonajava/introduction/Lesson04Operators.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.introduction;
+
+public class Lesson04Operators {
+    public static void main (String[] args){
+        int n1 = 10;
+        double n2 = 20;
+        double result = n1 / n2;
+        System.out.println("Value "+result);
+    }
+}
