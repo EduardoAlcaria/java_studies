7c2ad89dca756c8f66acc039915c019a0d242783
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-01-24 14:08:25 -0300

    remaking the folder structure

--- theory/HelloIntelij/src/academy/devdojo/maratonajava/introduction/Lesson01HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package academy.devdojo.maratonajava.introduction;
-
-public class Lesson01HelloWorld {
-    public static void main (String[] args){
-        System.out.println("Hello World");
-    }
-}
--- theory/HelloIntelij/src/academy/devdojo/maratonajava/introduction/Lesson05RelationalOperators.java
+++
@@ -1,8 +0,0 @@
-package academy.devdojo.maratonajava.introduction;
-
-public class Lesson05RelationalOperators {
-    public static void main (String[] args){
-        int oddOrEven = 21 % 2;
-        System.out.println(oddOrEven);
-    }
-}
