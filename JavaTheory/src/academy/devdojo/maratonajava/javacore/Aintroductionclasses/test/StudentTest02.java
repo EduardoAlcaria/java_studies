5ca07211f0036570324720730980d55e100f30b2
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-01-24 15:25:31 -0300

    studies about OOP (Object-oriented programming)

---
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/Aintroductionclasses/test/StudentTest02.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.javacore.Aintroductionclasses.test;
+
+import academy.devdojo.maratonajava.javacore.Aintroductionclasses.domain.student;
+
+public class StudentTest02 {
+    public static void main(String[] args) {
+       student student =  new student();
+        System.out.println(student.age);
+        System.out.println(student.name);
+        System.out.println(student.gen);
+    }
+}
