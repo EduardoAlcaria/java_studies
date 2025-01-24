bc0fb59c2808493f921344e3eda85e4426f7c745
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-01-24 10:56:55 -0300

    studies about Multidimensional arrays, part 1

--- theory/HelloIntelij/src/academy/devdojo/maratonajava/introduction/Lesson10MultidimensionalArrays01.java
+++ theory/HelloIntelij/src/academy/devdojo/maratonajava/introduction/Lesson10MultidimensionalArrays01.java
@@ -3,6 +3,13 @@ package academy.devdojo.maratonajava.introduction;
 public class Lesson10MultidimensionalArrays01 {
     public static void main(String[] args) {
         int[][] days = new int[3][3];
+        int counter = 1;
+        for (int i = 0; i < days.length; i++) {
+            for (int j = 0; j < days[i].length; j++) {
+                days[i][j] += counter;
+                counter++;
+            }
+        }
         for (int i = 0; i < days.length; i++) {
             for (int j = 0; j < days[i].length; j++) {
                 System.out.println(days[i][j]);
