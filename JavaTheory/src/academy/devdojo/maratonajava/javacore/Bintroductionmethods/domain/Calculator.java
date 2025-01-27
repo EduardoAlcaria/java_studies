73b6d2b589c4979a4cb6df079e5877ff48769af4
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-01-27 12:29:01 -0300

    studies about methods

---
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/Bintroductionmethods/domain/Calculator.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Bintroductionmethods.domain;
+
+public class Calculator {
+    public void addTwoNumbers() {
+        System.out.println(10 + 10);
+    }
+
+    public void subtractTwoNumbers() {
+        System.out.println(18 - 10);
+    }
+}
