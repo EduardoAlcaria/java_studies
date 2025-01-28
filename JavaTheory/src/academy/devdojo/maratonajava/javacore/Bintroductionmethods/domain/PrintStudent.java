71a122dad327ce8ee9e2eb1407c5627d01f0bb53
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-01-28 10:54:13 -0300

    studies about methods part 4

---
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/Bintroductionmethods/domain/PrintStudent.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Bintroductionmethods.domain;
+
+public class PrintStudent {
+    public void print(Student student) {
+        System.out.println("---------------------");
+        System.out.println(student.name);
+        System.out.println(student.age);
+        System.out.println(student.gen);
+    }
+}
+
---
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/Bintroductionmethods/tests/MethodsTests01.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.Bintroductionmethods.tests;
+
+import academy.devdojo.maratonajava.javacore.Bintroductionmethods.domain.PrintStudent;
+import academy.devdojo.maratonajava.javacore.Bintroductionmethods.domain.Student;
+
+public class MethodsTests01 {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        Student student2 = new Student();
+        PrintStudent printStudent = new PrintStudent();
+        student1.name = "Aizen";
+        student1.age = 35;
+        student1.gen = 'M';
+
+        student2.name = "kenpas";
+        student2.age = 45;
+        student2.gen = 'M';
+        printStudent.print(student1);
+        printStudent.print(student2);
+
+    }
+}
