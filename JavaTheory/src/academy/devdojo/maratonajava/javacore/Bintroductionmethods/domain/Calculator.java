4706826f92e189602965e63551b3e5bc6ac47d7f
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-01-28 08:50:58 -0300

    studies about methods (parameters)

--- JavaTheory/src/academy/devdojo/maratonajava/javacore/Bintroductionmethods/domain/Calculator.java
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/Bintroductionmethods/domain/Calculator.java
@@ -8,4 +8,7 @@ public class Calculator {
     public void subtractTwoNumbers() {
         System.out.println(18 - 10);
     }
+    public void times(int a, int b){
+        System.out.println(a * b);
+    }
 }
---
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/Bintroductionmethods/tests/MethodTests02.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Bintroductionmethods.tests;
+
+import academy.devdojo.maratonajava.javacore.Bintroductionmethods.domain.Calculator;
+
+public class MethodTests02 {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.times(2,2);
+    }
+}
