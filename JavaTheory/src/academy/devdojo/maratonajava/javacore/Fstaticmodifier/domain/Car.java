91a2fbe279a60aa0febd2081a91098d827669788
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-02-06 09:00:17 -0300

    going over through static methods

--- JavaTheory/src/academy/devdojo/maratonajava/javacore/Fstaticmodifier/domain/Car.java
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/Fstaticmodifier/domain/Car.java
@@ -5,39 +5,17 @@ public class Car {
     private double topSpeed;
     private static double speedLimit = 250;
 
-    public Car(String name, double topSpeed) {
+    public Car(String name, double topSpeed){
         this.name = name;
         this.topSpeed = topSpeed;
     }
-
-    public void print() {
-        System.out.println("------------------------");
-        System.out.println("Name " + this.name);
-        System.out.println("Top Speed " + this.topSpeed);
-        System.out.println("Speed Limit " + Car.speedLimit);
+    public void print(){
+        System.out.println("-----------------------------");
+        System.out.println("Name: " + this.name);
+        System.out.println("Top Speed: " + this.topSpeed);
+        System.out.println("Speed Limit:' " + Car.speedLimit);
     }
-
-    public static void setSpeedLimit(double speedLimit) {
+    public static void setSpeedLimit(double speedLimit){
         Car.speedLimit = speedLimit;
-    }
-
-    public static double getSpeedLimit(){
-        return Car.speedLimit;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getTopSpeed() {
-        return topSpeed;
-    }
-
-    public void setTopSpeed(double topSpeed) {
-        this.topSpeed = topSpeed;
     }
 }
--- JavaTheory/src/academy/devdojo/maratonajava/javacore/Fstaticmodifier/tests/StaticModifiersTest01.java
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/Fstaticmodifier/tests/StaticModifiersTest01.java
@@ -4,11 +4,13 @@ import academy.devdojo.maratonajava.javacore.Fstaticmodifier.domain.Car;
 
 public class StaticModifiersTest01 {
     public static void main(String[] args) {
+
         Car.setSpeedLimit(180);
 
-        Car car1 = new Car("BWM", 280);
-        Car car2 = new Car("Audi", 300);
+        Car car1 = new Car("Audi", 190);
+        Car car2 = new Car("BMW", 300);
         Car car3 = new Car("Mercedes", 310);
+
 
         car1.print();
         car2.print();
