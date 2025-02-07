042a508107195bd15eac5c7d09505b5e5818b313
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-02-07 12:22:55 -0300

    studies about reading values through Keyboard

---
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/Gassociation/tests/KeyboardInputTest01.java
@@ -0,0 +1,33 @@
+package academy.devdojo.maratonajava.javacore.Gassociation.tests;
+
+import java.util.Scanner;
+
+public class KeyboardInputTest01 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Type your name: ");
+        String name = input.nextLine();
+
+        System.out.print("Type your age: ");
+        int age = input.nextInt();
+
+        System.out.print("Type M for male or F for female: ");
+        char gen = input.next().charAt(0);
+
+        System.out.print(name + " is " + age + " years old ");
+
+        if ((gen == 'm') || (gen == 'M')){
+
+            System.out.print("and he is a male");
+
+        } else if ((gen == 'f') || (gen == 'F')) {
+
+            System.out.print("and she is a female");
+
+        }else{
+
+            System.out.println("\nError: the program does not recognize this gender");
+        }
+    }
+}
