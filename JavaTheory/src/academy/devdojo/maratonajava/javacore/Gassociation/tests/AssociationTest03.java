412ac563feddd7941afb8d63ca514fc4533a1112
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-02-07 11:11:23 -0300

    studies about Association

---
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/Gassociation/tests/AssociationTest03.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.javacore.Gassociation.tests;
+
+import academy.devdojo.maratonajava.javacore.Gassociation.domain.School;
+import academy.devdojo.maratonajava.javacore.Gassociation.domain.Teacher;
+
+public class AssociationTest03 {
+    public static void main(String[] args) {
+        Teacher teacher = new Teacher("satoru gojo");
+        Teacher teacher1 = new Teacher("Nanami");
+
+        Teacher[] teachers = {teacher, teacher1};
+
+        School school = new School("Jujutsu tech", teachers);
+
+        school.print();
+
+    }
+}
