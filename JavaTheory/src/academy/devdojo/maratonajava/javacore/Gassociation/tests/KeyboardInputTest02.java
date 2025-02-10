6e81b1557cc58364aa880a3d8d416404bb26c3c4
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-02-10 08:41:44 -0300

    Exercise about reading inputs

---
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/Gassociation/tests/KeyboardInputTest02.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.Gassociation.tests;
+
+import java.util.Scanner;
+
+public class KeyboardInputTest02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Type your question and i will answer with yes or no ");
+        System.out.print("Type your question: ");
+
+        String question = input.nextLine();
+
+
+        if ((question.charAt(0)) == 'a' || (question.charAt(0) == 'A')){
+            System.out.println("yes");
+        }else{
+            System.out.println("no");
+        }
+    }
+}
+
