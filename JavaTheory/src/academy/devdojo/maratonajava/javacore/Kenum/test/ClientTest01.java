c22c98a869f582bf6e3ea46c9bc90b705f3f6630
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-04-14 23:54:39 -0300

    Studies about using enumeration now working with contructors inside the class and using enumerations on a domain class

--- JavaTheory/src/academy/devdojo/maratonajava/javacore/Kenum/test/ClientTest01.java
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/Kenum/test/ClientTest01.java
@@ -6,12 +6,12 @@ import academy.devdojo.maratonajava.javacore.Kenum.domain.TypeClient;
 public class ClientTest01 {
     public static void main(String[] args) {
 
-        Client client1P = new Client("itadori", TypeClient.STUDENT, Client.PaymentMethod.CREDIT);
-        Client client2P = new Client("gojo", TypeClient.PROFESSOR, Client.PaymentMethod.DEBIT);
+        Client client1 = new Client("itadori", TypeClient.STUDENT, Client.PaymentMethod.CREDIT);
+        Client client2 = new Client("gojo", TypeClient.PROFESSOR, Client.PaymentMethod.DEBIT);
 
 
-        System.out.println(client1P);
-        System.out.println(client2P);
+        System.out.println(client1);
+        System.out.println(client2);
 
     }
 }
