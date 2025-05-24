7352f638f32061ace0d9be334e890515450fcf78
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-05-24 19:38:52 -0300

    learned about what is a runtime exception and the types unchecked and checked

---
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RunTimeExceptionTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Oexception.runtime.test;
+
+public class RunTimeExceptionTest01 {
+    public static void main(String[] args) {
+        Object obj = null;
+        int[] nums = {1,2};
+        System.out.println(obj.toString());
+
+        System.out.println(nums[2]);
+    }
+}
