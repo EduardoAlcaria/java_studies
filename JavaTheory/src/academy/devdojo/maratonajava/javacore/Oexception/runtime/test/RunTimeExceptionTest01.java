3ff2fef8a46b523e5d7259c075de3283919e8dbb
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-05-29 16:01:26 -0300

    Learned about the generical exception and the IOException, have learned how to treat exceptions with try and catch

--- JavaTheory/src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RunTimeExceptionTest01.java
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RunTimeExceptionTest01.java
@@ -3,9 +3,7 @@ package academy.devdojo.maratonajava.javacore.Oexception.runtime.test;
 public class RunTimeExceptionTest01 {
     public static void main(String[] args) {
         Object obj = null;
-        int[] nums = {1,2};
+        int[] nums = {1, 2};
         System.out.println(obj.toString());
-
-        System.out.println(nums[2]);
     }
 }
