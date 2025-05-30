7a0774776239430ab78b608a49bb9c4facc423b4
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-05-30 17:19:16 -0300

    learned about creating my own exceptions

---
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/Oexception/exception/test/InvalidLoginExceptionTest01.java
@@ -0,0 +1,30 @@
+package academy.devdojo.maratonajava.javacore.Oexception.exception.test;
+
+import academy.devdojo.maratonajava.javacore.Oexception.exception.domain.InvalidLoginException;
+
+import java.util.Scanner;
+
+public class InvalidLoginExceptionTest01 {
+    public static void main(String[] args) {
+        try {
+            login();
+        } catch (InvalidLoginException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void login() throws InvalidLoginException {
+        Scanner scanner = new Scanner(System.in);
+        String usernameDB = "Satoru Gojo";
+        String passwordDB = "purple";
+        System.out.println("user: ");
+        String usernameLocal = scanner.nextLine();
+        System.out.println("password: ");
+        String passwordLocal = scanner.nextLine();
+        if(!usernameLocal.equals(usernameDB) || !passwordLocal.equals(passwordDB)){
+            throw new InvalidLoginException("Invalid username or password");
+        }
+
+        System.out.println("Login successful");
+    }
+}
