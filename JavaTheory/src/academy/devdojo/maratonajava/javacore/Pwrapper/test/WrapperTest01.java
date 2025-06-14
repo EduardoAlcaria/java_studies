2bce9b6f4a9ed6d1bde61a1af2223423fa9a5b4f
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-06-14 20:35:41 -0300

    learned about wrappers and the autobox, unboxing, and the parse method

--- JavaTheory/src/academy/devdojo/maratonajava/javacore/Pwrapper/test/WrapperTest01.java
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/Pwrapper/test/WrapperTest01.java
@@ -20,6 +20,18 @@ public class WrapperTest01 {
         Character charW = 'W';
         Boolean booleanW = true;
 
+        int i = intW.byteValue(); //unboxing
+        Integer intW2 = Integer.parseInt("1");
+        boolean truee = Boolean.parseBoolean("TrUe");
+        System.out.println(truee);
 
+
+        System.out.println(Character.isDigit('A'));
+        System.out.println(Character.isDigit('9'));
+        System.out.println(Character.isLetterOrDigit('!'));
+        System.out.println(Character.isUpperCase('A'));
+        System.out.println(Character.isLowerCase('a'));
+        System.out.println(Character.toLowerCase('A'));
+        System.out.println(Character.toUpperCase('a'));
     }
 }
