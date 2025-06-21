cd20025a3075d07dcd0a17c625a79ce73911d470
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-06-21 18:15:42 -0300

    Studies about string and their performance with the String, StringBuilder and StringBuffer

---
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/Qstrings/test/StringPerformaceTest01.java
@@ -0,0 +1,46 @@
+package academy.devdojo.maratonajava.javacore.Qstrings.test;
+
+public class StringPerformaceTest01 {
+    public static void main(String[] args) {
+        long begin = System.currentTimeMillis();
+        concat(1_000_000);
+        long end = System.currentTimeMillis();
+
+        System.out.println("time for the concat String " + (end - begin) + "ms");
+
+        begin = System.currentTimeMillis();
+        concatStringBuilder(1_000_000);
+        end = System.currentTimeMillis();
+
+        System.out.println("time for the concat StringBuilder " + (end - begin) + "ms");
+
+begin = System.currentTimeMillis();
+        concatStringBuffer(1_000_000);
+        end = System.currentTimeMillis();
+
+        System.out.println("time for the concat StringBuffer " + (end - begin) + "ms");
+
+    }
+
+    private static void concatStringBuilder(int size){
+        StringBuilder sb = new StringBuilder(size);
+        for (int i = 0; i < size; i++) {
+            sb.append(i);
+        }
+
+    }
+    private static void concat(int size){
+        String txt = "";
+        for (int i = 0; i < size; i++) {
+            txt += i;
+        }
+
+    }
+    private static void concatStringBuffer(int size){
+        StringBuffer buffer = new StringBuffer(size);
+        for (int i = 0; i < size; i++) {
+            buffer.append(i);
+        }
+
+    }
+}
