84322edfc12698010b63f518b0cdf66138ecf03b
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-06-21 18:47:42 -0300

    Learned about the StringBuilder

---
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/Qstrings/test/StringBuilderTest01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Qstrings.test;
+
+public class StringBuilderTest01 {
+    public static void main(String[] args) {
+        String name = "Eduardo";
+        name.concat(" Alcaria");
+        System.out.println(name);
+        name.substring(0, 3);
+        StringBuilder sb = new StringBuilder("Eduardo");
+        sb.append(" Alcaria");
+        sb.delete(0, 3);
+        String ret = sb.substring(0, 2);
+        System.out.println(ret);
+        System.out.println(sb);
+    }
+}
