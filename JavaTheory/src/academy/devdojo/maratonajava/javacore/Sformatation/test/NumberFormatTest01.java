934fa71426df22793160485102b8f1fbea9dcab6
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-06-22 21:04:29 -0300

    Learned about how to format number based on country

---
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/Sformatation/test/NumberFormatTest01.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.javacore.Sformatation.test;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class NumberFormatTest01 {
+    public static void main(String[] args) {
+        Locale localeDefault = Locale.getDefault();
+        Locale localeBR = new Locale("pt", "BR");
+        Locale localeJP = Locale.JAPAN;
+        Locale localeIT = Locale.ITALY;
+
+
+        NumberFormat[] numberFormat = new NumberFormat[4];
+
+        numberFormat[0] = NumberFormat.getInstance();
+        numberFormat[1] = NumberFormat.getInstance(localeJP);
+        numberFormat[2] = NumberFormat.getInstance(localeBR);
+        numberFormat[3] = NumberFormat.getInstance(localeIT);
+
+        double value = 10_000.1231;
+
+        for (NumberFormat format : numberFormat) {
+            System.out.println(format.format(value));
+        }
+
+
+    }
+}
