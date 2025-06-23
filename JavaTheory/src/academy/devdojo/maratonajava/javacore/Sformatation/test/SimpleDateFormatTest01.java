7c9826e15e15ae513e72f276e1980d0206457ce2
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-06-22 21:27:59 -0300

    Learned about the simple format

---
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/Sformatation/test/SimpleDateFormatTest01.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.javacore.Sformatation.test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SimpleDateFormatTest01 {
+    public static void main(String[] args) {
+        String pattern = "'Brazil' dd MMMM 'of' yyyy HH:mm:ss z";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        System.out.println(simpleDateFormat.format(new Date()));
+    }
+}
