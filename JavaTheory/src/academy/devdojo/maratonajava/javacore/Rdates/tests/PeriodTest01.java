9751e98ddf7f9a337cd25f9fbf6f6df750119cba
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-06-27 23:08:50 -0300

    Learned about the Period class

---
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/Rdates/tests/PeriodTest01.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.Rdates.tests;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+
+public class PeriodTest01 {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        Period p1 = Period.between(date, date.plusYears(7));
+        Period p2 = Period.ofDays(10);
+        Period p3 = Period.ofWeeks(58);
+        Period p4 = Period.ofMonths(3);
+        Period p5 = Period.ofYears(3);
+
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+        System.out.println(p4);
+        System.out.println(p5);
+    }
+}
