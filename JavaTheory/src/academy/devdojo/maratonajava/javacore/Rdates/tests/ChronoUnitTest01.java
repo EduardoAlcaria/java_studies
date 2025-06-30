709c82773aa0b34233f964e6a4a8af77130296d3
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-06-30 19:46:02 -0300

    Learned about the ChronoUnit class

---
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/Rdates/tests/ChronoUnitTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.Rdates.tests;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
+
+
+public class ChronoUnitTest01 {
+    public static void main(String[] args) {
+        LocalDateTime birthDay = LocalDateTime.of(2006, Month.NOVEMBER, 25, 15, 0, 0);
+        System.out.println(ChronoUnit.YEARS.between(birthDay, LocalDateTime.now()));
+
+        LocalDate timeUntilBirthDay = LocalDate.of(LocalDate.now().getYear(), Month.NOVEMBER, 25);
+        System.out.println(ChronoUnit.DAYS.between(LocalDate.now(), timeUntilBirthDay));
+    }
+}
