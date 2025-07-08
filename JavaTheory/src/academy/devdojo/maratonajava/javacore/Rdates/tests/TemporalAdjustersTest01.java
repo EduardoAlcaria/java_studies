aa91bf31110404a78f6d9f142f87d4fc0c1f2da7
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-07-08 10:38:37 -0300

    Learned about the TemporalAdjusters

---
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/Rdates/tests/TemporalAdjustersTest01.java
@@ -0,0 +1,42 @@
+package academy.devdojo.maratonajava.javacore.Rdates.tests;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+
+public class TemporalAdjustersTest01 {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+
+        now = now.withDayOfMonth(8);
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(TemporalAdjusters.next(DayOfWeek.TUESDAY));
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+
+        now = LocalDate.now().with(TemporalAdjusters.previous(DayOfWeek.TUESDAY));
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(TemporalAdjusters.firstDayOfMonth());
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(TemporalAdjusters.lastDayOfMonth());
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(TemporalAdjusters.lastDayOfMonth());
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().withMonth(11).withDayOfMonth(25);
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+
+    }
+}
