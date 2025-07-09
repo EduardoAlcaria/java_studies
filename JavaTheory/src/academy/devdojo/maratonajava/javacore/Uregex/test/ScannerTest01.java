50a2cc21638575f1532495bc1e7b0e47e41530d5
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-07-09 11:47:37 -0300

    learned about scanners

---
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/Uregex/test/ScannerTest01.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.javacore.Uregex.test;
+
+public class ScannerTest01 {
+    public static void main(String[] args) {
+        String text = "Satoru gojo, Itadori, true, 200";
+        String[] names = text.split(",");
+        for (String name : names) {
+            System.out.println(name.trim());
+        }
+
+    }
+}
---
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/Uregex/test/ScannerTest02.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.javacore.Uregex.test;
+
+import java.util.Scanner;
+
+public class ScannerTest02 {
+    public static void main(String[] args) {
+        String text = "Satorugojo,Itadori,true,200";
+        Scanner scanner = new Scanner(text);
+        scanner.useDelimiter(",");
+        while (scanner.hasNext()){
+            if (scanner.hasNextInt()){
+                int i = scanner.nextInt();
+                System.out.println("int " + i);
+            } else if (scanner.hasNextBoolean()) {
+                boolean b = scanner.nextBoolean();
+                System.out.println("boolean " + b);
+            }else{
+                System.out.println(scanner.next());
+            }
+        }
+
+    }
+}
