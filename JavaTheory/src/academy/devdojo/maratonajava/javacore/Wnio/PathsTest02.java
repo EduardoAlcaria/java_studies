aa645432c24d5638b7172f1d02b94824bb9b4a2e
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-07-10 14:16:07 -0300

    learned about the nio and the paths and Files classes

---
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/Wnio/PathsTest02.java
@@ -0,0 +1,32 @@
+package academy.devdojo.maratonajava.javacore.Wnio;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+public class PathsTest02 {
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("folder");
+        if (Files.notExists(path)){
+            Path path1 = Files.createDirectory(path);
+        }
+
+        Path subDirPath = Paths.get("folder\\subdir\\subsubdir");
+        Path subDir = Files.createDirectories(subDirPath);
+        Path filePath = Paths.get(subDirPath.toString(), "subFile.txt");
+
+        if (Files.notExists(filePath)){
+            Path filePath1 = Files.createFile(filePath);
+        }
+
+
+        Path source = filePath;
+        Path target = Paths.get(filePath.getParent().toString(), "renamedSubFile.txt");
+        Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
+
+
+    }
+}
