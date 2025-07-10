d8d2ed149c43b0b9f1d32e1abf2c74f157a934fc
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-07-10 12:31:52 -0300

    learned about the old io and the file class

--- JavaTheory/src/academy/devdojo/maratonajava/javacore/Vio/test/FileTest01.java
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/Vio/test/FileTest01.java
@@ -7,7 +7,21 @@ public class FileTest01 {
     public static void main(String[] args) throws IOException {
         File file = new File("file1.txt");
         File file2 = new File("D:\\linux\\programming\\studies\\java_studies\\JavaTheory\\file2.txt");
-        System.out.println(file.createNewFile());
-        System.out.println(file2.createNewFile());
+        try {
+            System.out.println("isCreatedFile1: " + file.createNewFile());
+            System.out.println("isCreatedFile2: " + file2.createNewFile());
+
+            System.out.println("path1" + file.getAbsolutePath());
+            System.out.println("Path2" + file2.getAbsolutePath());
+
+            System.out.println("does file1 exists: " + file.exists());
+            System.out.println("does file2 exists: " + file2.exists());
+
+            System.out.println("does file could be deleted: " + file2.delete());
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
