99a16b2859d81eee402d84ae79a72edc90d0dea9
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-07-10 14:38:14 -0300

    learned about the relativize method

---
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/Wnio/RelativizePathsTest01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Wnio;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class RelativizePathsTest01 {
+    public static void main(String[] args) {
+        Path abolute = Paths.get("/javaTheory/folder");
+        Path file = Paths.get("/javaTheory/folder/subdir/subsubdir/subFile.txt");
+
+        System.out.println(abolute.relativize(file));
+    }
+}
