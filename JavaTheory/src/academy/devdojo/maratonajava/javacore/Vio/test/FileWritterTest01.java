9bac2fae98455f7260561765d40b0eea4e166cd4
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-07-10 12:32:37 -0300

    learned about the non-buffered fileWriter

---
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/Vio/test/FileWritterTest01.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.javacore.Vio.test;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWritterTest01 {
+    public static void main(String[] args){
+
+        try (FileWriter writer = new FileWriter("file1.txt", true)){
+            writer.write("Bankai\n");
+            writer.flush();
+
+        }catch (IOException ioException){
+            throw new RuntimeException();
+        }
+
+    }
+}
