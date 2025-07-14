6e93ac245587b1ba9efe5247791cb1b2e23d14b7
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-07-14 11:39:10 -0300

    Exercise about the PathMatcher

---
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/Wnio/PathMacherExercise01.java
@@ -0,0 +1,35 @@
+package academy.devdojo.maratonajava.javacore.Wnio;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+class GetAllFiles extends SimpleFileVisitor<Path>{
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs){
+        boolean isMatch = getFiles(file);
+        if (isMatch){
+            System.out.println(file.getFileName());
+        }
+        return FileVisitResult.CONTINUE;
+    }
+
+
+    public static boolean getFiles(Path file){
+        String glob = "glob:*{Test*}.{java,class}";
+        PathMatcher matcher = FileSystems.getDefault().getPathMatcher(glob);
+        if (matcher.matches(file.getFileName())){
+            return true;
+        }
+        return false;
+
+    }
+}
+
+
+public class PathMacherExercise01 {
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get(".");
+        Files.walkFileTree(path, new GetAllFiles());
+    }
+}
