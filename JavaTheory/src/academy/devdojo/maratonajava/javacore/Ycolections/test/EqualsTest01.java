f6d3d053723e355bb7fc2a63328689a4e5628f80
EduardoAlcaria <eduardoalcarialopes@gmail.com>
2025-07-29 18:03:23 -0300

    learned about equals from the collections framework

---
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/Ycolections/test/EqualsTest01.java
@@ -0,0 +1,13 @@
+package JavaTheory.src.academy.devdojo.maratonajava.javacore.Ycolections.test;
+
+import JavaTheory.src.academy.devdojo.maratonajava.javacore.Ycolections.domain.Smartphone;
+
+public class EqualsTest01 {
+    public static void main(String[] args) {
+        Smartphone s1 = new Smartphone("1ABC1", "Samsung");
+        Smartphone s2 = new Smartphone("1ABC1", "Samsung");
+
+        System.out.println(s1.equals(s2));
+
+    }
+}
