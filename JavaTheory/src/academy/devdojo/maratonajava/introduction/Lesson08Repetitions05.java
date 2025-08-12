d5d3708caffa7edee334f3b95a8d77038b8139f8
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-08-12 15:09:22 -0300

    fixed some package's names

--- JavaTheory/src/academy/devdojo/maratonajava/introduction/Lesson08Repetitions05.java
+++ JavaTheory/src/academy/devdojo/maratonajava/introduction/Lesson08Repetitions05.java
@@ -1,4 +1,4 @@
-package academy.devdojo.maratonajava.introduction;
+package JavaTheory.src.academy.devdojo.maratonajava.introduction;
 
 public class Lesson08Repetitions05 {
     public static void main(String[] args) {
--- JavaTheory/src/academy/devdojo/maratonajava/introduction/Lesson09Arrays02.java
+++ JavaTheory/src/academy/devdojo/maratonajava/introduction/Lesson09Arrays02.java
@@ -1,4 +1,4 @@
-package academy.devdojo.maratonajava.introduction;
+package JavaTheory.src.academy.devdojo.maratonajava.introduction;
 
 public class Lesson09Arrays02 {
     public static void main(String[] args) {
