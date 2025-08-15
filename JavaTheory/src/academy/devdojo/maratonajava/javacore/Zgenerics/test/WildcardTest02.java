33fa94dc34b16c7e9e713c231c0fc3267d4ef5d7
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-08-15 14:36:27 -0300

    learned about wildcards pt2

--- JavaTheory/src/academy/devdojo/maratonajava/javacore/Zgenerics/test/WildcardTest02.java
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/Zgenerics/test/WildcardTest02.java
@@ -1,5 +1,6 @@
 package JavaTheory.src.academy.devdojo.maratonajava.javacore.Zgenerics.test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class WildcardTest02 {
@@ -7,13 +8,24 @@ public class WildcardTest02 {
         List<Dog> dogs = List.of(new Dog(), new Dog());
         List<Cat> cats = List.of(new Cat(), new Cat());
 
+        printAppointment(cats);
+        printAppointment(dogs);
+
+        List<Animal> animals = new ArrayList<>();
+        printAppointment(animals);
+
+
     }
 
-    private static void printAppointment(List<Animal> animals){
+    private static void printAppointment(List<? extends Animal> animals){
         for (Animal animal : animals) {
             animal.appointment();
         }
 
+    }
+
+    private static void printAppointmentAnimal(List<? super Animal> animals){
         animals.add(new Dog());
+        animals.add(new Cat());
     }
 }
