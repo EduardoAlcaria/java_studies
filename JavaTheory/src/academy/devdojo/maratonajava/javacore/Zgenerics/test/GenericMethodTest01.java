e90d7da5a420e27745047ecfce3a658d166d18d2
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-08-15 16:37:45 -0300

    learned about generic methods

---
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/Zgenerics/test/GenericMethodTest01.java
@@ -0,0 +1,17 @@
+package JavaTheory.src.academy.devdojo.maratonajava.javacore.Zgenerics.test;
+
+import JavaTheory.src.academy.devdojo.maratonajava.javacore.Zgenerics.domain.Boat;
+
+import java.util.List;
+
+public class GenericMethodTest01 {
+    public static void main(String[] args) {
+        List<Boat> coolBoat = createArrayWithObj(new Boat("Cool Boat"));
+        System.out.println(coolBoat);
+    }
+
+    private static <T> List<T> createArrayWithObj(T t){
+        return List.of(t);
+    }
+}
+
