094d6094e7be82a123198c6ad12e71906de54b58
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-08-20 14:56:49 -0300

    learned about using method reference now making a reference to the constructor

---
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/ZZClambdas/test/MethodReferenceTest04.java
@@ -0,0 +1,48 @@
+package JavaTheory.src.academy.devdojo.maratonajava.javacore.ZZClambdas.test;
+
+import JavaTheory.src.academy.devdojo.maratonajava.javacore.ZZClambdas.domain.Anime;
+import JavaTheory.src.academy.devdojo.maratonajava.javacore.ZZClambdas.service.AnimeComparator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.*;
+
+public class MethodReferenceTest04 {
+    public static void main(String[] args) {
+        Supplier<AnimeComparator> newAnimeComparator = AnimeComparator::new;
+        AnimeComparator animeComparator = newAnimeComparator.get();
+        List<Anime> animes = new ArrayList<>(List.of(new Anime("jujutsu kaisen", 55), new Anime("blue lock", 33)));
+
+        Collections.sort(animes, AnimeComparator::compareByEpisodes);
+        animes.sort(animeComparator::compareByEpisodesNonStatic);
+
+        BiFunction<String, Integer, Anime> animeBiFunction = (s, n) -> new Anime(s, n);
+        BiFunction<String, Integer, Anime> animeBiFunction1 = Anime::new;
+
+        Anime blueLock = animeBiFunction.apply("Blue Lock", 32);
+        Anime jujutsuKaisen = animeBiFunction1.apply("Jujutsu Kaisen", 40);
+
+        System.out.println(animes);
+
+        System.out.println(blueLock);
+        System.out.println(jujutsuKaisen);
+
+        Function<String, Integer> parseInt = Integer::parseInt;
+        Integer apply = parseInt.apply("10");
+
+        System.out.println(apply);
+
+
+        animes.sort((a1, a2) -> a1.getName().compareTo(a2.getName()));
+        System.out.println(animes);
+        animes.sort(AnimeComparator::compareByEpisodes);
+        System.out.println(animes);
+
+
+
+
+
+
+    }
+}
