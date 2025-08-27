dece2b7b487bc3ad02d931bf7ac09ee4a8021f1a
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-08-27 18:21:21 -0300

    learned about using Streams

---
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/ZZEstreams/test/StreamTest05.java
@@ -0,0 +1,23 @@
+package JavaTheory.src.academy.devdojo.maratonajava.javacore.ZZEstreams.test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamTest05 {
+    public static void main(String[] args) {
+        List<String> words = List.of("Satoru", "aizen", "Itadori", "eu");
+        String[] split = words.get(0).split("");
+        System.out.println(Arrays.toString(split));
+
+       List<String[]> stream = words.stream().map(l -> l.split("")).collect(Collectors.toList());
+        List<String> collect = words.stream()
+                .map(w -> w.split(""))
+                .flatMap(Arrays::stream)
+                .collect(Collectors.toList());
+
+
+        System.out.println(collect);
+    }
+}
