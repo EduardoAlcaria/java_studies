5cb3c58187459f42c211bed2e8bae556207d4b91
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-08-29 23:37:44 -0300

    learned about using Streams

---
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/ZZEstreams/test/StreamTest07.java
@@ -0,0 +1,22 @@
+package JavaTheory.src.academy.devdojo.maratonajava.javacore.ZZEstreams.test;
+
+import java.util.List;
+import java.util.Optional;
+
+public class StreamTest07 {
+    public static void main(String[] args) {
+        List<Integer> integers = List.of(1, 2, 3, 4, 5, 6);
+        integers.stream().reduce((x, y) -> x  + y)
+                .ifPresent(System.out::println);
+
+
+        Optional<Integer> reduce = integers.stream().reduce(Integer::sum);
+        System.out.println(reduce.get());
+
+
+        integers.stream().reduce((x, y) -> x > y ? x : y).ifPresent(System.out::println);
+        System.out.println(integers.stream().reduce(Integer::max));
+        System.out.println(integers.stream().reduce(10, Integer::max));
+    }
+}
+
