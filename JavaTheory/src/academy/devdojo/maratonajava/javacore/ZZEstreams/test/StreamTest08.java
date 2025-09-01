7a600e7f61ae9aab61757a04c317cad2f7d08e3d
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-09-01 09:34:37 -0300

    did an exercise about streams and making a sum with streams, also learned about using mapToDouble

---
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/ZZEstreams/test/StreamTest08.java
@@ -0,0 +1,32 @@
+package JavaTheory.src.academy.devdojo.maratonajava.javacore.ZZEstreams.test;
+
+import JavaTheory.src.academy.devdojo.maratonajava.javacore.ZZEstreams.demon.LightNovel;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamTest08 {
+    private static ArrayList<LightNovel> lightNovels = new ArrayList<>(List.of(
+            new LightNovel("solo leveling", 1),
+            new LightNovel("bleach", 2),
+            new LightNovel("naruto", 2),
+            new LightNovel("dbz", 1),
+            new LightNovel("Kagurabachi", 6),
+            new LightNovel("Jujutsu Kaisen", 31),
+            new LightNovel("Chainsaw man", 6)
+    ));
+    public static void main(String[] args) {
+            lightNovels.stream()
+                .map(LightNovel::getPrice)
+                .filter(price -> price > 3)
+                .reduce(Double::sum)
+                .ifPresent(System.out::println);
+
+        double sum = lightNovels.stream()
+                .mapToDouble(LightNovel::getPrice)
+                .filter(price -> price > 3)
+                .sum();
+        System.out.println(sum);
+    }
+}
