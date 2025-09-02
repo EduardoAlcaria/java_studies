c7f213eb77d9bedc0e4b3f210ca9a8d0ee98b139
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-09-02 12:02:17 -0300

    learned about using Collectors and its methods, such as summarizing, summing and some other methods to operate values

---
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/ZZEstreams/test/StreamTest11.java
@@ -0,0 +1,69 @@
+package JavaTheory.src.academy.devdojo.maratonajava.javacore.ZZEstreams.test;
+
+import JavaTheory.src.academy.devdojo.maratonajava.javacore.ZZEstreams.demon.LightNovel;
+
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class StreamTest11 {
+    private static ArrayList<LightNovel> lightNovels = new ArrayList<>(List.of(
+            new LightNovel("solo leveling", 1),
+            new LightNovel("bleach", 2),
+            new LightNovel("naruto", 2),
+            new LightNovel("dbz", 1),
+            new LightNovel("Kagurabachi", 6),
+            new LightNovel("Jujutsu Kaisen", 31),
+            new LightNovel("Chainsaw man", 6)
+    ));
+    public static void main(String[] args) {
+       lightNovels.stream()
+               .map(LightNovel::getPrice)
+               .filter(l -> l > 3)
+               .reduce(Double::sum)
+               .ifPresent(System.out::println);
+
+       lightNovels.stream()
+               .map(LightNovel::getPrice)
+               .max(Double::compareTo)
+               .ifPresent(System.out::println);
+
+
+       lightNovels.stream()
+               .map(LightNovel::getTitle)
+               .map(String::toLowerCase)
+               .filter(l -> l.endsWith("kaisen"))
+               .forEach(System.out::println);
+
+
+       lightNovels.stream().max(Comparator.comparing(LightNovel::getPrice)).ifPresent(System.out::println);
+
+       lightNovels.stream().collect(Collectors.maxBy(Comparator.comparing(LightNovel::getPrice))).ifPresent(System.out::println);
+
+        double sum = lightNovels.stream().mapToDouble(LightNovel::getPrice).sum();
+        System.out.println(sum);
+
+        System.out.println(lightNovels.stream().collect(Collectors.summingDouble(LightNovel::getPrice)));
+
+        OptionalDouble avg = lightNovels.stream().mapToDouble(LightNovel::getPrice).average();
+        System.out.println(lightNovels.stream().collect(Collectors.averagingDouble(LightNovel::getPrice)));
+
+
+        DoubleSummaryStatistics collect = lightNovels.stream().collect(Collectors.summarizingDouble(LightNovel::getPrice));
+
+        System.out.println(collect);
+
+
+
+        String collect1 = lightNovels.stream()
+                .map(LightNovel::getTitle)
+                .sorted(String::compareToIgnoreCase)
+                .collect(Collectors.joining(", "));
+
+        System.out.println(collect1);
+
+
+
+    }
+
+}
