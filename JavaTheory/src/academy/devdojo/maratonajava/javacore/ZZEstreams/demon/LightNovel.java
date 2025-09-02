05a809452d51ef63e1f8d00d6a8df3a7841ec7ee
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-09-02 12:17:18 -0300

    learned about using Collectors and its methods, such as Grouping by

--- JavaTheory/src/academy/devdojo/maratonajava/javacore/ZZEstreams/demon/LightNovel.java
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/ZZEstreams/demon/LightNovel.java
@@ -5,6 +5,8 @@ import java.util.Objects;
 public class LightNovel {
     private String title;
     private double price;
+    private Category category;
+
 
     public LightNovel(String title, double price) {
         this.title = title;
@@ -12,11 +14,18 @@ public class LightNovel {
     }
 
 
+    public LightNovel(String title, double price, Category category) {
+        this.title = title;
+        this.price = price;
+        this.category = category;
+    }
+
     @Override
     public String toString() {
         return "LightNovel{" +
                 "title='" + title + '\'' +
                 ", price=" + price +
+                ", category=" + category +
                 '}';
     }
 
@@ -46,5 +55,13 @@ public class LightNovel {
 
     public void setPrice(double price) {
         this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
     }
 }
