3145c0e6404bf9af7c8b379d76ff079ca9d99679
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-09-08 15:07:20 -0300

    learned about using Threads and its methods such as join and yield

---
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/ZZFtheads/test/ThreadTest02.java
@@ -0,0 +1,34 @@
+package JavaTheory.src.academy.devdojo.maratonajava.javacore.ZZFtheads.test;
+
+
+class ThreadExempleRunnable2 implements Runnable{
+
+    private final String c;
+
+    public ThreadExempleRunnable2(String c){
+        this.c = c;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+        for (int i = 0; i < 1000; i++) {
+            System.out.print(c);
+            if (i % 100 == 0){
+                System.out.println();
+            }
+        }
+        Thread.yield();
+
+    }
+}
+
+public class ThreadTest02 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(new ThreadExempleRunnable2("KA"));
+        Thread t2 = new Thread(new ThreadExempleRunnable2("ME"));
+        t1.start();
+        t1.join();
+        t2.start();
+    }
+}
