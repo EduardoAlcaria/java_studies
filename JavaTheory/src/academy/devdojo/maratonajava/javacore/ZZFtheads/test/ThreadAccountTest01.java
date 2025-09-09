f05404d6d377b36022785f6e534648bf02ea1753
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-09-09 07:51:58 -0300

    learned about using Threads and how to use synchronized to control the flow of the threads

--- JavaTheory/src/academy/devdojo/maratonajava/javacore/ZZFtheads/test/ThreadAccountTest01.java
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/ZZFtheads/test/ThreadAccountTest01.java
@@ -3,7 +3,7 @@ package JavaTheory.src.academy.devdojo.maratonajava.javacore.ZZFtheads.test;
 import JavaTheory.src.academy.devdojo.maratonajava.javacore.ZZFtheads.domain.Account;
 
 public class ThreadAccountTest01 implements Runnable{
-    private Account account = new Account();
+    private final Account account = new Account();
     public static void main(String[] args) {
         ThreadAccountTest01 threadAccountTest01 = new ThreadAccountTest01();
         Thread t1 = new Thread(threadAccountTest01, "Zaraki");
@@ -17,20 +17,26 @@ public class ThreadAccountTest01 implements Runnable{
     @Override
     public void run() {
         for (int i = 0; i < 5; i++) {
+
             withdrawal(10.0);
             if (account.getBalance() < 0){
                 System.out.println("WE ARE NOW POOR");
             }
         }
     }
+    private static synchronized void print(){
+        System.out.println("java is cool");
 
+    }
     private void withdrawal(double amount){
-        if (account.getBalance() >= amount){
-            System.out.println(getName() + " is going to withdrawal " + amount + "$");
-            account.withdrawl(amount);
-            System.out.println(getName() + " account balance is now " + account.getBalance() + "$");
-        }else{
-            System.out.println("there is no money to withdrawal for " + getName() + " account");
+        synchronized (account) {
+            if (account.getBalance() >= amount) {
+                System.out.println(getName() + " is going to withdrawn money");
+                account.withdrawl(amount);
+                System.out.println(getName() + " account balance is now " + account.getBalance() + "$");
+            } else {
+                System.out.println("there is no money to withdrawal for " + getName() + " account");
+            }
         }
     }
 
