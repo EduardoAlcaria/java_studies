d4e7649252cbca9c5e316d4fefe7fb9c40cb3ab5
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-09-23 09:53:51 -0300

    learned about restraint lock and how does it work using lock.tryLock and lock.lock and lock.unlock in the finally block and some useful methods

---
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/ZZGConcurrency/test/ReentrantLockTest01.java
@@ -0,0 +1,49 @@
+package JavaTheory.src.academy.devdojo.maratonajava.javacore.ZZGConcurrency.test;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+class Worker implements Runnable{
+    private String name;
+    private ReentrantLock lock;
+
+    public Worker(String name, ReentrantLock lock) {
+        this.name = name;
+        this.lock = lock;
+    }
+
+    @Override
+    public void run() {
+        lock.lock();
+        try {
+            if (lock.isHeldByCurrentThread()){
+                System.out.printf("Thread %s has entered in a critical state%n", name);
+            }
+            System.out.printf("There are %d Threads waiting on the queue %n", lock.getQueueLength());
+            System.out.printf("Thread %s will wait for 2 segs %n", name);
+            Thread.sleep(2000);
+            System.out.printf("Thread %s is now free%n",name);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+
+}
+
+public class ReentrantLockTest01 {
+    public static void main(String[] args) {
+        ReentrantLock lock = new ReentrantLock(true);
+        new Thread(new Worker("A", lock)).start();
+        new Thread(new Worker("B", lock)).start();
+        new Thread(new Worker("C", lock)).start();
+        new Thread(new Worker("D", lock)).start();
+        new Thread(new Worker("E", lock)).start();
+        new Thread(new Worker("F", lock)).start();
+        new Thread(new Worker("G", lock)).start();
+
+
+
+    }
+}
