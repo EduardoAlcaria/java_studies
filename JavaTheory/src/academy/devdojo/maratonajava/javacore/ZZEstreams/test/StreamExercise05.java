7d3633e6d1f157b5fef780870c0237d4d96d03c7
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-10-28 22:36:37 -0300

    Remembering about how to streams

---
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/ZZEstreams/test/StreamExercise05.java
@@ -0,0 +1,25 @@
+package JavaTheory.src.academy.devdojo.maratonajava.javacore.ZZEstreams.test;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamExercise05 {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>(
+                List.of("Buda", "Gojo", "Aizen", "Eduardo")
+        );
+        List<String> sorted = list.stream()
+                .sorted()
+                .toList();
+
+        System.out.println(sorted);
+
+        System.out.println("==============================");
+
+
+
+    }
+
+
+}
