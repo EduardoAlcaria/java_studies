6112686f58c1d0dc9ee6312f867aa14a78174bce
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2025-10-29 23:28:16 -0300

    Remembering about how to use streams

--- JavaTheory/src/academy/devdojo/maratonajava/javacore/ZZEstreams/test/StreamExercise09.java
+++ JavaTheory/src/academy/devdojo/maratonajava/javacore/ZZEstreams/test/StreamExercise09.java
@@ -1,15 +1,15 @@
 package JavaTheory.src.academy.devdojo.maratonajava.javacore.ZZEstreams.test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.DoubleSummaryStatistics;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
 
 public class StreamExercise09 {
     public static void main(String[] args) {
-        System.out.println("==============================");
+
 
         List<String> list = new ArrayList<>(
                 List.of("Buda", "Gojo", "Aizen", "Eduardo")
